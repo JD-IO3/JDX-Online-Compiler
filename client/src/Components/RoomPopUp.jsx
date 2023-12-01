5b3c03b8329915fd39f388a2f32d6de49646f05b
JaydeepKhandla <jaydeepkhandla01@gmail.com>
2023-12-01 16:12:32 +0530

    update_01-12-23

--- client/src/Components/RoomPopUp.jsx
+++ client/src/Components/RoomPopUp.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import {toast, Toaster} from 'react-hot-toast';
+import { toast, Toaster } from 'react-hot-toast';
 import { v4 as uuidV4 } from 'uuid';
 import './Styles/roompopup.css';
 
-function RoomPopUp({ setPopUp, username, setUserName, setPlaygroundId, playgroundId  }) {
+function RoomPopUp({ setPopUp, username, setUserName, setPlaygroundId, playgroundId }) {
 
   const navigate = useNavigate();
 
@@ -13,21 +13,24 @@ function RoomPopUp({ setPopUp, username, setUserName, setPlaygroundId, playgroun
   }
 
   const handleJoinPlayground = () => {
-    if(!playgroundId || !username){
+
+    if (!playgroundId || !username) {
       toast('You Forgot Username or Playground Id?', {
         duration: 1300,
         position: 'top-center',
-  
+
         icon: '🤨'
       })
     }
 
     //?Redirect
-    navigate(`/playground/${playgroundId}`, {
-      state: {
-        username
-      }
-    })
+    if (playgroundId && username) {
+      navigate(`/playground/${playgroundId}`, {
+        state: {
+          username
+        }
+      })
+    }
 
   }
 
@@ -66,14 +69,14 @@ function RoomPopUp({ setPopUp, username, setUserName, setPlaygroundId, playgroun
             <button className='close-btn' onClick={handleCloseClick} > X </button>
           </div>
           <div className="input-box-container">
-            <input type="text" className='roomId-ip' placeholder='Enter Playground Id' onChange={ (e) => setPlaygroundId((prevplaygroundId) => prevplaygroundId = e.target.value) } value={ playgroundId } />
-            <input type="text" className='name-ip' placeholder='Enter Your Name' onChange={ (e) => setUserName((prevusername) => prevusername = e.target.value) } value={ username } />
+            <input type="text" className='roomId-ip' placeholder='Enter Playground Id' onChange={(e) => setPlaygroundId((prevplaygroundId) => prevplaygroundId = e.target.value)} value={playgroundId} />
+            <input type="text" className='name-ip' placeholder='Enter Your Name' onChange={(e) => setUserName((prevusername) => prevusername = e.target.value)} value={username} />
           </div>
           <div className="join-btn-container">
             <button className='join-btn' onClick={handleJoinPlayground} >Join Playground</button>
           </div>
           <div className="create-room-text">
-            <p>Doesn't have a Playground Id? <br /> <br /> Create a New, <a onClick={ handleNewPgClick } href='#'>Playground</a></p>
+            <p>Doesn't have a Playground Id? <br /> <br /> Create a New, <a onClick={handleNewPgClick} href='#'>Playground</a></p>
           </div>
         </div>
       </div>
